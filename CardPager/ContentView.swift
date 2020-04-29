e1c811d806dcd4b8d2eba532003f39d5f979a1b3
Rod Landaeta <rodrigoelp@gmail.com>
2020-04-29 17:55:13 +1000

    Creating sample content

--- CardPager/ContentView.swift
+++ CardPager/ContentView.swift
@@ -4,7 +4,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Group { Text("Page 0") }
+                .asCard(backgroundColor: .purple)
+
+            Group { Text("Page 1") }
+                .asCard(backgroundColor: .blue)
+
+            Group { Text("Page 2") }
+                .asCard(backgroundColor: .green)
+
+            Group { Text("Page 3") }
+                .asCard(backgroundColor: .orange)
+
+            Group { Text("Page 4") }
+                .asCard(backgroundColor: .yellow)
+
+            Group { Text("Page 5") }
+                .asCard(backgroundColor: .red)
+
+            Group { Text("Page 6") }
+                .asCard(backgroundColor: .purple)
+        }
+    }
+}
+
+extension View {
+    func asCard(backgroundColor: Color, foregroundColor: Color = .white) -> some View {
+        self
+            .frame(width: 300, height: 500)
+            .background(backgroundColor)
+            .foregroundColor(foregroundColor)
+            .cornerRadius(20)
     }
 }
 
